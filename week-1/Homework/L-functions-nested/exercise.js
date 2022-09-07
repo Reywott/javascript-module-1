b0068b44ffda7b95197505dbd423ee6690127beb
Rey Wot <rewat1991213gmail.com>
2022-09-07 10:48:31 +0200

    finally finished week 1

--- week-1/Homework/L-functions-nested/exercise.js
+++ week-1/Homework/L-functions-nested/exercise.js
@@ -3,3 +3,31 @@ var mentor2 = "Irina";
 var mentor3 = "Mimi";
 var mentor4 = "Rob";
 var mentor5 = "Yohannes";
+
+/*function shoutyGreeting() {
+    const greeting = "HELLO"
+    console.log(greeting, mentor1.toUpperCase());
+    console.log(greeting, mentor2.toUpperCase());
+    console.log(greeting, mentor3.toUpperCase());
+    console.log(greeting, mentor4.toUpperCase());
+    console.log(greeting, mentor5.toUpperCase()); 
+    
+}                              
+
+shoutyGreeting();  */   //first solution
+
+
+function logElement(mentor){
+    console.log("HELLO", mentor.toUpperCase());
+}
+
+
+
+
+function toGreet(){
+    const mentors = [mentor1, mentor2, mentor3, mentor4, mentor5];
+    const greeting = "HELLO"
+    //mentors.forEach(mentor => console.log(greeting, mentor.toUpperCase()));   //alternate solution
+    mentors.forEach(logElement);
+}
+toGreet();
